380c74df84a8e094fba89e443620182724bdf050
paullaster <paullasterokoth98@gmail.com>
2022-10-27 07:26:27 +0300

    adding handler methods for input n button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+const initialList = [
+  {
+    name: 'Kennedy',
+    ID: 1,
+  },
+  {
+    name: 'John',
+    ID: 2,
+  },
+  {
+    name: 'Phonfred',
+    ID: 3,
+  }
+];
+
 function App() {
-  const namee = [
-    {
-      name: 'Kennedy',
-      ID: 1,
-    },
-    {
-      name: 'John',
-      ID: 2,
-    },
-    {
-      name: 'Phonfred',
-      ID: 3,
-    }
-  ]
+  const [list, setList] = useState (initialList);
+
+  const handleChange = (event) => {
+
+  };
+
+  const handleAdd = (event) => {};
+
   return (
     <div className="App">
       <h1> We want to manipulate list </h1>
+      <div>
+        <input type="text" id="list" placeholder="add to list" />
+        <button type="button">Add</button>
+      </div>
       <ul>
-        {namee.map ( (item) => {
+        {list.map ( (item) => {
           return (
-            <li className='list-item'>{item.name}</li>
+            <li className='list-item' key={item.ID}>{item.name}</li>
           )
         })}
       </ul>
